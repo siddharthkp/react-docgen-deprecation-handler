d7329c708bb77b9d24ed283e43709b4d959cb36f
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2019-01-15 17:09:27 +0530

    add extended data parsing

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+const pattern = /@deprecated\:\w*/
+
 function deprecationHandler(documentation) {
   for (var [key, value] of documentation._props.entries()) {
-    if (value.description.includes("@deprecated ")) {
+    if (value.description.includes("@deprecated")) {
       value.deprecated = true
-      value.description = value.description.replace("@deprecated ", "")
+
+      /* default expected value of annotation */
+      let deprecatedAnnotation = "@deprecated "
+
+      /* extended metadata format: @deprecated:metadata */
+      const match = value.description.match(pattern)
+      if (match) {
+        deprecatedAnnotation = match[0]
+        value.deprecationData = deprecatedAnnotation.replace("@deprecated:", "")
+      }
+
+      /* strip annotation */
+      value.description = value.description.replace(deprecatedAnnotation, "")
     }
   }
 }
--- test.js
+++ test.js
@@ -14,18 +14,22 @@ const code = `
     /** @deprecated dont use this prop */
     old: PropTypes.any,
     /** this isn't deprecated, use it */
-    new: PropTypes.any
+    new: PropTypes.any,
+    /** @deprecated:meta with metadata */
+    extended: PropTypes.any,
   }
 
   export default Component
 `
 
 const data = docgen.parse(code, null, handlers)
-console.log(data.props.old)
 
 test("Old prop should have deprecated set to true", t => {
   t.is(data.props.old.deprecated, true)
 })
 test("New prop should not have deprecated key", t => {
   t.is(data.props.new.deprecated, undefined)
+})
+test("extended prop should have metadata", t => {
+  t.is(data.props.extended.deprecationData, "meta")
 })
